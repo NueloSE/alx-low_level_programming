7ae40dd522a04b8d7accf99311d4bf832ec916e2
Nuelo <akalo.emmanuel18@gmail.com>
2023-08-15 05:59:34 +0100

    upload task 11

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/syscall.h>
+
+/**
+ * custom_print - A function that prints to the std output
+ * @str: A point to the string to be printed
+ * Return: Always 0 on success
+ */
+
+void custom_print(const char *str)
+{
+	syscall(SYS_write, STDERR_FILENO, str, strlen(str));
+}
+
+/**
+ * main - Entry point of the program
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	const char *text = "and that piece of art is "
+		"useful\" - Dora korpar, 2015-10-19\n";
+
+	custom_print(text);
+	return (1);
+}
