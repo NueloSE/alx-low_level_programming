99f7c729eafe4aea1e05ae46cecabd43e98e6ab1
Nuel <akalo.emmanuel18@gmail.com>
2023-08-21 10:41:11 +0000

    Upload task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets a variable to 98
+ * @n: the variable to change the value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo contains 12 task to show understanding of pointers in C
