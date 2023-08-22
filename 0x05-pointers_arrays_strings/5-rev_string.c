3bf871498ea9df3ec4aae29e8414972aa7bc78a3
Nuel <akalo.emmanuel18@gmail.com>
2023-08-22 11:10:50 +0000

    Upload task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, j = 0, i;
+	char tmp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len = len - 1;
+	i = len / 2;
+	for (; j < i; j++, len--)
+	{
+		tmp = s[j];
+		s[j] = s[len];
+		s[len] = tmp;
+	}
+}
