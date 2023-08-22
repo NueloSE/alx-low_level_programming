178503ad9ee61f6709521a081cddad4b34966c69
Nuel <akalo.emmanuel18@gmail.com>
2023-08-22 15:19:10 +0000

    upload task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string to standard out using a space of two
+ * @str: the string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
