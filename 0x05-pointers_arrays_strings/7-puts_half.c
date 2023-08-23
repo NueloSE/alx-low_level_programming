8b7f2cc856dc8b54e854dd1ac6f263bf6960ae40
Nuel <akalo.emmanuel18@gmail.com>
2023-08-23 09:38:20 +0000

    Upload task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string to be printed
+ * Rrturn: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, half;
+
+	while (str[len] != '\0')
+		len++;
+	half = len / 2;
+	while (half < len)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
