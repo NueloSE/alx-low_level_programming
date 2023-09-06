f8a31aaef647ae46978e60e9574251b9cddd9a9b
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-06 19:06:36 +0100

    Fix wrong memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = (char *) malloc(size * sizeof(int));
+	ptr = (char *) malloc(size * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	for (; i < size; i++)
