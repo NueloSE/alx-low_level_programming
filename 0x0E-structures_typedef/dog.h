7c013e96da7ce78adbcada6c0dea53ad51f744ef
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-11 19:05:55 +0100

    Update readme.md and dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A user defined data type
+ * @name: Name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+
 struct dog
 {
 	char *name;
