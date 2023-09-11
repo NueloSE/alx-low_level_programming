7f496acbdff9b7392245cb4050f71d5e3abccc22
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-11 13:35:55 +0100

    Upload task 0 to 4 and a readme file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "0-object_like_macro.h"
+
+/**
+ * main - Entry point, Expands the macros __FILE__
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
