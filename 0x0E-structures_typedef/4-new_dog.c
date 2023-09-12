973acb8c81e0847f3daf581d055138766d371e1c
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-12 09:56:48 +0100

    Fix copying of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copies a string
+ * @dest: The final string
+ * @src: The copied string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - A function that creates a new dog
  * @name: pointer to the name of the dog
  * @age: Dog age
@@ -15,9 +32,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return (NULL);
-	dog1->name = name;
-	dog1->age = age;
-	dog1->owner = owner;
 
+	dog1->name = malloc(sizeof(name) + 1);
+	if (dog1->name == NULL)
+	{
+		free(dog1);
+		return (NULL);
+	}
+	_strcpy(dog1->name, name);
+
+	dog1->owner = malloc(sizeof(owner) + 1);
+	if (dog1->owner == NULL)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+	_strcpy(dog1->owner, owner);
+	dog1->age = age;
 	return (dog1);
 }
