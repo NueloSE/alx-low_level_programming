875b26714215b374b699e5df043cb79b8549e516
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-23 14:05:38 +0100

    fix output error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,6 @@
 #include <unistd.h>
 #include <string.h>
-#include <sys/syscall.h>
 
-/**
- * custom_print - A function that prints to the std output
- * @str: A point to the string to be printed
- * Return: Always 0 on success
- */
-
-void custom_print(const char *str)
-{
-	if (str != NULL)
-		syscall(SYS_write, STDERR_FILENO, str, strlen(str));
-}
 
 /**
  * main - Entry point of the program
@@ -25,6 +13,6 @@ int main(void)
 
 	text = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
 
-	custom_print(text);
+	write(1, text, 59);
 	return (1);
 }
