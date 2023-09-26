754887f16929a02fea52093034478944f8b8b240
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-26 15:06:54 +0100

    Upload task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: a double pointer to the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((*head)->next != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	free(*head);
+}
