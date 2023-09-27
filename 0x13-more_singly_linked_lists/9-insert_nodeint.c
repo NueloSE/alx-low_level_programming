30f7fc89ee8d6f80fcd8d8ea7ec58d43adc610f6
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-27 16:10:55 +0100

    Fix insertion problem

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,37 +2,33 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: address of the list
- * @idx: the position
- * @n: the data
- * Return: a pointer to the newnode on Success. Otherwise NULL
+ * @head: pointer to the list
+ * @idx: the index
+ * @n: the data to be added
+ * Return: the address of the new node on Success. Otherwise NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newnode, *currnode;
-	unsigned int i = 0;
+	unsigned int num = 1;
+	listint_t *tmp = *head, *newnode;
 
-	if (!head)
-		return (NULL);
+	while (num < idx)
+	{
+		if (tmp == NULL)
+			return (NULL);
 
-	currnode = *head;
+		tmp = tmp->next;
+		num++;
+	}
 
 	newnode = malloc(sizeof(listint_t));
+
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-	newnode->next = NULL;
-	idx = ((idx > 0) ? (idx - 1) : 0);
-	while (i != idx && currnode)
-	{
-		currnode = currnode->next;
-		i++;
-	}
+	newnode->next = tmp->next;
+	tmp->next = newnode;
 
-	if (idx > i)
-		return (NULL);
-	newnode->next = currnode->next;
-	currnode->next = newnode;
 	return (newnode);
 }
