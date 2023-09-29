636285795764bd0c1a2789912bf6f72bb2c38650
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-29 13:03:32 +0100

    Update functionality

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,10 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0' || b[i] == '1')
+			res = (res << 1) | (b[i] - '0');
+		else
 			return (0);
-		res = (res << 1) | (b[i] - '0');
 	}
 	return (res);
 }
