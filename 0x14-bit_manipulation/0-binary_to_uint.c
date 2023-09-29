e298f98da771592378c1edb9529cd035c09c2547
Nuelo <akalo.emmanuel18@gmail.com>
2023-09-29 13:17:51 +0100

    Quick fix of binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int i;
+	unsigned int result = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
@@ -18,14 +18,12 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
-			res = (res << 1);
+			result = result << 1;
 			if (b[i] == '1')
-			{
-				res = res | 1;
-			}
+				result = result | 1;
 		}
 		else
 			return (0);
 	}
-	return (res);
+	return (result);
 }
