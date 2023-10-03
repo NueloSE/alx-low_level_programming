4e0a41f5e052f94715b885713af3f9dbfadcef24
Nuelo <akalo.emmanuel18@gmail.com>
2023-10-03 19:22:48 +0100

    fix passing NULL as text content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 
 	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
 		return (-1);
 
 	while (text_content[i] != '\0')
@@ -24,8 +27,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	if (text_content != NULL)
-		write(fd, text_content, i);
+	write(fd, text_content, i);
 
 	close(fd);
 
