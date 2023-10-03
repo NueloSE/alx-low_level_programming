ca0ad5abe61a8eb7504b4a55891a77350d3e6de7
Nuelo <akalo.emmanuel18@gmail.com>
2023-10-03 19:12:27 +0100

    Fix NULL text passed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,10 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	write(fd, text_content, i);
+	if (text_content != NULL)
+		write(fd, text_content, i);
+
+	close(fd);
 
 	return (1);
 }
