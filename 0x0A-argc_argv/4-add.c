b6fcc602be4297446e2d5423ff5d0495a8d65536
Nuelo <akalo.emmanuel18@gmail.com>
2023-11-18 01:48:12 +0100

    Fix string handling

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,24 +9,26 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0, num, i;
+	int result = 0, i, j;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
-		return (1);
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-		if (num)
-			result += num;
-		else
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		result += atoi(argv[i]);
 	}
+
 	printf("%d\n", result);
 	return (0);
 }
