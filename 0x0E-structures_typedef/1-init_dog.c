040df556e571edca10a7af0ca3d115c873bda0a7
Nuelo <akalo.emmanuel18@gmail.com>
2023-11-27 16:25:44 +0100

    Upload task one

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: a pointer to the dog struct
+ * @name: name of dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - a secondary data structure with the following elements:
  * @name: name of dog
@@ -15,7 +17,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-void int_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
