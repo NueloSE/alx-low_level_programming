cbd90754730f65be7887200fc157c4f9f8dd6b4e
Nuelo <akalo.emmanuel18@gmail.com>
2023-11-30 16:56:02 +0100

    upload task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: a separator between each string
+ * @n: the number of element in the string
+ * Return: Nothing
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list mylist;
+
+	if (n == 0)
+		return;
+	va_start(mylist, n);
+	if (strcmp(separator, "NULL") == 0)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(mylist, int));
+		if (i + 1 != n)
+		{
+			printf("%s ", separator);
+		}
+	}
+	printf("\n");
+	va_end(mylist);
+}
