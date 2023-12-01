dbda583049607752f8bec03d2872b386dc7b8760
Nuelo <akalo.emmanuel18@gmail.com>
2023-12-01 20:48:53 +0100

    Fix handling of non NULL byte

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i <= 5)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
 	printf("Error\n");
-	exit(99);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,16 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
 	char *sign;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	int (*call_func)(int, int);
 
 	if (argc != 4)
 	{
@@ -29,14 +22,29 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	sign = argv[2];
 
-	if (!strchr("+-*/%", *sign))
+	if (((strcmp(sign, "/") == 0) || (strcmp(sign, "%") == 0)) && (num2 == 0))
 	{
 		printf("Error\n");
-		exit(98);
+		return (100);
 	}
 
-	result = get_op_func(sign)(num1, num2);
+	call_func = get_op_func(sign);
 
-	printf("%d\n", result);
+	if (call_func == NULL)
+		exit(99);
+	printf("%d\n", call_func(num1, num2));
+	exit(0);
+	/**
+	 * if (!strchr("+-*%", *sign))
+	*{
+	*	printf("Error\n");
+	*	exit(98);
+	*}
+
+
+	*result = get_op_func(sign)(num1, num2);
+
+	*printf("%d\n", result);
 	return (0);
+	*/
 }
