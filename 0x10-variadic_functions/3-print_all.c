7402ceb9350cdd25bbae4fe129777944f5705bd9
Nuelo <akalo.emmanuel18@gmail.com>
2023-12-02 05:16:40 +0100

    Upload task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: is a list of types of arguments passed to the function
+ * Return: void
+*/
+
+void print_all(const char *format, ...)
+{
+	int i = 0, j = 0;
+	va_list ap;
+	handlers printers[] = {
+		{'i', handle_int},
+		{'c', handle_char},
+		{'s', handle_string},
+		{'f', handle_float},
+		{'0', NULL},
+	};
+
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (printers[j].str != 0)
+		{
+			if (printers[j].str == format[i])
+			{
+				printers[j].printer(ap);
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
+
+
+/**
+ * handle_int - prints integer value to stdout
+ * @ap: a variadic list
+ * Return: void
+*/
+
+void handle_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * handle_char - prints character to the stdout
+ * @ap: a variadic list
+ * Return: void
+*/
+
+void handle_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * handle_float - prints float value to the stdout
+ * @ap: a variadic list
+ * Return: void
+*/
+
+void handle_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * handle_string - prints string to the stdout
+ * @ap: variadic list
+ * Return: void
+*/
+
+void handle_string(va_list ap)
+{
+	printf("%s", va_arg(ap, char *));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,10 +4,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct handler - prints different format specifiers
+ * @str: a charcter that specifies the format
+ * @printer: function pointer to the function that prints a specific format
+*/
+typedef struct handler
+{
+	char str;
+	void (*printer)(va_list);
+} handlers;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+void handle_int(va_list);
+void handle_char(va_list);
+void handle_float(va_list);
+void handle_string(va_list);
 
 #endif /* VARIADIC_FUNCTIONS_H*/
