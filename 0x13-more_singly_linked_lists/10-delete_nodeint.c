501cb6804a615feebb04e1f0b2bedf5d6af3a68f
Nuelo <akalo.emmanuel18@gmail.com>
2023-12-13 20:03:16 +0100

    Fix size of loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int node = 0;
+	unsigned int node = 1;
 	listint_t *curr_node = *(head), *prev_node = *(head);
 
 	if (head == NULL || *head == NULL)
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(curr_node);
 		return (1);
 	}
-	while (node < index)
+	while (node <= index)
 	{
 		if ((node == index) && (curr_node != NULL))
 		{
