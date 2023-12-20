1ad634362199758a3f1f5aa0c31fad45a40d2980
Nuelo <akalo.emmanuel18@gmail.com>
2023-12-20 17:36:48 +0100

    Quick fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,14 +18,23 @@ int error_handler(int num, int exit_val, char *filename)
 		case (2):
 			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", filename);
 			return (exit_val);
-		case (3):
-			dprintf(STDERR_FILENO, "%sCan't close fd  %d\n", filename, exit_val);
-			return (100);
-		case (4):
-			dprintf(STDERR_FILENO, "%sCan't close fd  %d\n", filename, exit_val);
-			return (100);
 	}
 	return (1);
+}
+
+/**
+ * closing - closes a file descriptor
+ * @fd: the file discriptor
+ * Return: the exit value
+*/
+
+void closing(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
@@ -52,7 +61,7 @@ int main(int ac, char **av)
 	{
 		exit(error_handler(1, 98, file_from));
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		exit(error_handler(2, 99, file_to));
@@ -66,13 +75,7 @@ int main(int ac, char **av)
 		if (write(fd_to, buffer, reading) == -1)
 			exit(error_handler(2, 99, file_from));
 	} while (true);
-	if (close(fd_from) == -1)
-	{
-		exit(error_handler(4, fd_from, "Error: "));
-	}
-	if (close(fd_to) == -1)
-	{
-		exit(error_handler(3, fd_to, "Error: "));
-	}
+	closing(fd_from);
+	closing(fd_to);
 	return (0);
 }
