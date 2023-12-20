5d23b93518ceb7cb282d62cbcb4391ee9cd658d9
Nuelo <akalo.emmanuel18@gmail.com>
2023-12-20 17:23:42 +0100

    Fix 5 pending checkers

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,78 @@
 #include "main.h"
 
 /**
- * main - entry point
- * @ac: number of passed argument
- * @av: passed argument
- * Return: 0 on success.
- */
+ * error_handler - prints the appropriate err message
+ * @num: case specifier
+ * @exit_val: the exit value
+ * @filename: name of the file
+ * Return: the exit value passed
+*/
+
+int error_handler(int num, int exit_val, char *filename)
+{
+	switch (num)
+	{
+		case (1):
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+			return (exit_val);
+		case (2):
+			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", filename);
+			return (exit_val);
+		case (3):
+			dprintf(STDERR_FILENO, "%sCan't close fd  %d\n", filename, exit_val);
+			return (100);
+		case (4):
+			dprintf(STDERR_FILENO, "%sCan't close fd  %d\n", filename, exit_val);
+			return (100);
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point for the code
+ * @ac: argument count
+ * @av: array of variadic
+ * Return: always 0
+*/
+
 int main(int ac, char **av)
 {
-	int fd1, fd2, n_read;
-	char *write_err = "Error: Can't write to file ",
-	*read_err = "Error: Can't read from ";
-	char buffer[BUFF_SIZE], *close_err = "Error: Can't close fd ";
+	char buffer[1024];
+	int fd_from, fd_to, reading = 0;
+	char *file_from, *file_to;
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
 	}
-	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
+	file_from = av[1], file_to = av[2];
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
+		exit(error_handler(1, 98, file_from));
 	}
-	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (fd2 == -1)
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "%s%s\n", write_err, av[2]), exit(99);
+		exit(error_handler(2, 99, file_to));
 	}
-	while ((n_read = read(fd1, buffer, BUFF_SIZE)) > 0)
+	do {
+		reading = read(fd_from, &buffer, 1024);
+		if (reading == 0)
+			break;
+		else if (reading == -1)
+			exit(error_handler(1, 98, file_from));
+		if (write(fd_to, buffer, reading) == -1)
+			exit(error_handler(2, 99, file_from));
+	} while (true);
+	if (close(fd_from) == -1)
 	{
-		if ((write(fd2, buffer, n_read)) < n_read)
-		{
-			dprintf(STDERR_FILENO, "%s%s\n", write_err, av[2]), exit(99);
-		}
+		exit(error_handler(4, fd_from, "Error: "));
 	}
-	if (n_read == -1)
+	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "%s%s\n", read_err, av[2]), exit(99);
-	}
-	if (close(fd1) != 0)
-	{
-		dprintf(STDERR_FILENO, "%s%d\n", close_err, fd1), exit(100);
-	}
-	if (close(fd2) != 0)
-	{
-		dprintf(STDERR_FILENO, "%s%d\n", close_err, fd2), exit(100);
+		exit(error_handler(3, fd_to, "Error: "));
 	}
 	return (0);
 }
