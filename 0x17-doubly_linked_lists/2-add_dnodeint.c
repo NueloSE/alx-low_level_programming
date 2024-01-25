53eb802244aba0bbf6ec5e98077987f6d46dfbf4
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-25 12:55:41 +0100

    Fix NULL header checker

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,13 +16,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 
-	if (head == NULL || (*head) == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
-	}
 	new_node->prev = NULL;
 	new_node->next = (*head);
 	*head = new_node;
