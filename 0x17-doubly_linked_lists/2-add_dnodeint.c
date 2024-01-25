3106952826ba667d7b07499f0e962fc4f656fbbe
Nuelo <akalo.emmanuel18@gmail.com>
2024-01-25 12:32:08 +0100

    Quick fix NULL header passed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 
-	if ((*head) == NULL)
+	if (head == NULL || (*head) == NULL)
 	{
 		new_node->next = NULL;
 		new_node->prev = NULL;
