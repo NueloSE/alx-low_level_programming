3710c22e3fa534f9d4aff3c0972617704135268c
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-15 11:37:00 +0100

    feat: create a custom header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALOGS_H
+#define SEARCH_ALOGS_H
+
+/*INCLUDE STANDARD LIBRARY HEADERS*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/*prototypes*/
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /*SEARCH_ALOGS_H*/
