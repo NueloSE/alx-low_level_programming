c1b18bd79bb091e52c87b2b66b4fe7ba2cddc6bd
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-17 18:01:39 +0100

    feat: create a script performing interpolation sort

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in a sorted array
+ * @array: pointer to the first element of the array to search
+ * @size: the number of element in array
+ * @value: the value to search for
+ * Return: first index where value is located on success. Otherwise -1
+*/
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size -1;
+	size_t pos;
+
+	if (size == 0 || array == NULL)
+		return (-1);
+
+	while (low <= high )
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			low = pos + 1;
+		else {
+			high = pos - 1;
+			if (high < low){
+				printf("Value checked array[%d] is out of range\n", 2109);
+				return (-1);}
+			}
+	}
+	printf("Value checked array[%d] is out of range\n", 2109);
+	return (-1);
+}
