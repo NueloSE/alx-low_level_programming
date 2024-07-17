9be2d7dcc17e9f60bb457dd87f9de851f600f889
Nuelo <akalo.emmanuel18@gmail.com>
2024-07-17 19:53:20 +0100

    fix: betty complian for header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,11 +9,11 @@
 
 /**
  * struct listint_s - singly linked list
- * 
+ *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- * 
+ *
  * Description: singly linked list node structure
 */
 
